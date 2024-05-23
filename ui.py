6dd7ff1637c7821f89c7e162c0b7b05798983b21
developer1 <devl@email.com>
2024-05-23 11:31:08 +0900

    Add lbl1 in yi.py to print current date

--- ui.py
+++ ui.py
@@ -1,13 +1,19 @@
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout)   # 애플리케이션 핸들러와 빈 GUI 위젯
-from PyQt5.QtGui import QIcon                       # icon을 추가하기 위한 라이브러리
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout, QLabel)   # 애플리케이션 핸들러와 빈 GUI 위젯
+
+from PyQt5.QtGui import QIcon 
+from PyQt5.QtCore import QDate, Qt                     # icon을 추가하기 위한 라이브러리
 
 class View(QWidget):
       
     def __init__(self):
         super().__init__()                          # 부모 클래스 Qwidget을 초기화
+        self.date = QDate.currentDate()
         self.initUI()                               # 나머지 초기화는 initUI 함수의 정의
     
     def initUI(self):         
+
+        self.lbl1 = QLabel(self.date.toString(Qt.DefaultLocaleLongDate))
+        self.te1 = QPlainTextEdit
 
         self.te1=QPlainTextEdit()
         self.te1.setReadOnly(True)
@@ -23,7 +29,7 @@ class View(QWidget):
         vbox = QVBoxLayout()                        # 수직 레이아웃 위젯 생성
         vbox.addWidget(self.te1)
         vbox.addLayout(hbox)
-        vbox.addStretch(1)                          # 빈 공간
+        vbox.addWidget(self.lbl1)
 
         self.setLayout(vbox)                        # 빈 공간 - 버튼 - 빈 공간 순으로 수직 배치된 레이아웃 설정
 

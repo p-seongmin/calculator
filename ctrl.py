1aa13056c6078039aa7f9ff918b96a369b25aeb9
developer1 <devl@email.com>
2024-06-11 17:56:36 +0900

    Remove pow.mod functions and operators

--- ctrl.py
+++ ctrl.py
@@ -28,20 +28,3 @@ class Control:
     def mul(self, a, b):
         return a*b
     
-    def div(self, a, b):
-        try: 
-            if (b==0):
-                raise Exception("Divisor Error")
-        except Exception as e:
-            return e
-        return a/b
-    
-    def pow(self, a, b):
-        try:
-            if (a==0):
-                raise Exception("Base Error")
-            
-        except Exception as e:
-            return e
-    
-        return pow(a, b)
